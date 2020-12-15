b99b4660ac028509e40911caff348cfbfee02b12
sdimitro <sdimitro@eecs.wsu.edu>
2020-12-14 23:12:01 -0800

    Update TestChainingHash.h

--- src/TestChainingHash.h
+++ src/TestChainingHash.h
@@ -6,23 +6,26 @@
 
 class TestChainingHash {
     private:
-        ChainingHash<string, int>* table;
-
-        void pretest(string, string, string);
-        void posttest(int, int, bool, bool, bool);
-        bool verify(int, int, bool);
-        string statusStr(Status);
-
+        ChainingHash<string, int>* table; //pointer to dynamically allocated hash table
+        
         enum Status {
             FAILED = 0,
             PASSED = 1,
             SEE_OUTPUT = 2
-        };
+        }; //private enumerated type used to show unit test result - see TestChainingHash.cpp for more details
+
+        //private helper functions 
+        void pretest(string, string, string);
+        void posttest(int, int, bool, bool, bool);
+        bool verify(int, int, bool);
+        string statusStr(Status);
         
     public:
+        //constructor and destructor
         TestChainingHash();
         ~TestChainingHash();
 
+        //public unit tests
         void testInsert();
         void testEmplace();
         void testAt();
